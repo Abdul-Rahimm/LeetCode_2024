5b9945210a4d495523076677155af6cb9c750692
Abdul-Rahimm <k213951@nu.edu.pk>
2024-02-20 18:32:52 +0500

    qs7 started

---
+++ 2. February/AI Assignment 01/qs7.cpp
@@ -0,0 +1,31 @@
+// بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef array<int, 2> ii;
+typedef vector<ii> vii;
+const int mod = 1e9 + 7;
+
+void solve();
+
+signed main(void)
+{
+    // ios::sync_with_stdio(false);
+    // cin.tie(nullptr);
+    int tc = 1;
+    // cin >> tc;
+
+    while (tc--)
+        solve();
+
+    return 0;
+}
+
+void solve()
+{
+}
