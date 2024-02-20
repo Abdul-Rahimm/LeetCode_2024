ff3fc3b88bb5d5c3f1a315776f0b784e87535fc2
Abdul-Rahimm <k213951@nu.edu.pk>
2024-02-20 19:49:40 +0500

    done with N queens

--- 2. February/AI Assignment 01/qs7.cpp
+++ 2. February/AI Assignment 01/qs7.cpp
@@ -12,7 +12,6 @@ typedef vector<ii> vii;
 const int mod = 1e9 + 7;
 
 void solve();
-
 signed main(void)
 {
     // ios::sync_with_stdio(false);
@@ -26,6 +25,97 @@ signed main(void)
     return 0;
 }
 
+void solve1(vector<vector<int>> &safe, int n, int row, int c)
+{
+    for (int i = 0; i < n; i++)
+    {
+        safe[row][i]++;
+    }
+    for (int i = row + 1; i < n; i++)
+    {
+        safe[i][c]++;
+        if (c + i - row < n)
+            safe[i][c + i - row]++;
+        if (c - i + row >= 0)
+            safe[i][c - i + row]++;
+    }
+}
+// This function is used to backtrack
+void solve2(vector<vector<int>> &safe, int n, int row, int c)
+{
+    for (int i = 0; i < n; i++)
+    {
+        safe[row][i]--;
+    }
+    for (int i = row + 1; i < n; i++)
+    {
+        safe[i][c]--;
+        if (c + i - row < n)
+            safe[i][c + i - row]--;
+        if (c - i + row >= 0)
+            safe[i][c - i + row]--;
+    }
+}
+void solve(int n, vector<vector<string>> &ans, vector<vector<int>> &safe, int row, vector<string> &tempo)
+{
+    if (row == n)
+    {
+        ans.push_back(tempo);
+        return;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (safe[row][i] == 0)
+        {
+            solve1(safe, n, row, i);
+            tempo[row][i] = 'Q';
+            solve(n, ans, safe, row + 1, tempo);
+            tempo[row][i] = '.';
+            solve2(safe, n, row, i);
+        }
+    }
+}
+vector<vector<string>> solveNQueens(int n)
+{
+    vector<vector<int>> safe;
+    vector<vector<string>> ans;
+    vector<string> tempo;
+    for (int i = 0; i < n; i++)
+    {
+        vector<int> temp;
+        string a;
+        for (int j = 0; j < n; j++)
+        {
+            a = a + '.';
+            temp.push_back(0);
+        }
+        tempo.push_back(a);
+        safe.push_back(temp);
+    }
+    int row = 0;
+    solve(n, ans, safe, row, tempo);
+    return ans;
+}
+
+void print(vector<vector<string>> &ans)
+{
+    for (vector<string> row : ans)
+    {
+        for (string i : row)
+        {
+            cout << i << " ";
+        }
+
+        cout << endl
+             << endl;
+    }
+}
+
 void solve()
 {
+    int n = 4;
+    cin >> n;
+
+    vector<vector<string>> ans = solveNQueens(n);
+    print(ans); // prints the chess board
 }
