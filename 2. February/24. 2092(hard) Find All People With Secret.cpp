ebaf0de8fa14df3789832813d04b301e031b328f
Abdul-Rahimm <k213951@nu.edu.pk>
2024-02-25 00:49:43 +0500

    couldnt solve it on own

--- 2. February/24. 2092(hard) Find All People With Secret.cpp
+++ 2. February/24. 2092(hard) Find All People With Secret.cpp
@@ -5,7 +5,7 @@ using namespace std;
 typedef vector<int> vi;
 typedef vector<vi> vii;
 
-vector<int> findAllPeople(int n, vector<vector<int>> &nums, int firstPerson)
+vector<int> findAllPeople(int numberPeople, vector<vector<int>> &nums, int firstPerson)
 {
     auto comparater = [](vector<int> &row1, vector<int> &row2) -> bool
     {
@@ -14,6 +14,9 @@ vector<int> findAllPeople(int n, vector<vector<int>> &nums, int firstPerson)
     sort(nums.begin(), nums.end(), comparater);
 
     int n = nums.size();
+    bool status;
+
+    vector<int> tempValues;
     vector<int> ans;
     set<int> st;
 
@@ -23,6 +26,33 @@ vector<int> findAllPeople(int n, vector<vector<int>> &nums, int firstPerson)
     for (int i = 0; i < n; i++)
     {
         vector<int> row = nums[i];
+        status = false;
+
+        int j = i + 1;
+        while (j < n - 1 && row[2] == nums[j][2])
+        {
+            // push in tempValues and then clear tempValues
+            tempValues.push_back(nums[j][1]);
+            tempValues.push_back(nums[j][0]);
+
+            if (st.find(nums[j][1]) != st.end() || st.find(nums[j][0]) != st.end())
+            {
+                status = true;
+            }
+
+            j++;
+            i++;
+        }
+
+        if (status)
+        {
+            for (int i : tempValues)
+            {
+                st.insert(i);
+            }
+        }
+
+        tempValues.clear();
 
         if (st.find(row[0]) != st.end() || st.find(row[1]) != st.end())
         {
@@ -38,7 +68,6 @@ vector<int> findAllPeople(int n, vector<vector<int>> &nums, int firstPerson)
 
     return ans;
 }
-
 void print(vector<vector<int>> nums)
 {
     for (vector<int> row : nums)
@@ -48,9 +77,14 @@ void print(vector<vector<int>> nums)
         cout << endl;
     }
 }
+void print(vector<int> nums)
+{
+    for (int i : nums)
+        cout << i << " ";
+}
 int main()
 {
     vector<vector<int>> nums{{1, 2, 5}, {2, 3, 4}, {1, 5, 7}};
-
-    print(nums);
+    vector<int> ans = findAllPeople(6, nums, 1);
+    print(ans);
 }
