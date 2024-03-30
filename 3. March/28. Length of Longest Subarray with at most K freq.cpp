b6b6b61ffda423fce499a291edd10ab7ee3162f0
Abdul-Rahimm <k213951@nu.edu.pk>
2024-03-30 14:09:01 +0500

    minor changes after dry run. it works now

--- 3. March/28. Length of Longest Subarray with at most K freq.cpp
+++ 3. March/28. Length of Longest Subarray with at most K freq.cpp
@@ -1,29 +1,32 @@
-int maxSubarrayLength(vector<int>& nums, int k) {
-        unordered_map<int, int> mp;
-        
-        int i = 0;
-        int j = 0;
-        int n = nums.size();
-        int ans = 0;
-        
-        
-        while(j < n){
-            
-            if(mp[nums[j]] <= k){
-                //increase the window size to allow more elements to come in
-                mp[nums[j]]++;
-                j++;
-            }else{
-                while(nums[i] != nums[j])
-                    i++;
-                
-                mp[nums[j]]--;
-                i++;
-            }
-            
-            ans = max(ans, j-i+1);
-            
+int maxSubarrayLength(vector<int> &nums, int k)
+{
+    unordered_map<int, int> mp;
+
+    int i = 0;
+    int j = 0;
+    int n = nums.size();
+    int ans = 0;
+
+    while (j < n)
+    {
+
+        if (mp[nums[j]] < k)
+        {
+            // increase the window size to allow more elements to come in
+            mp[nums[j]]++;
+            j++;
         }
-        
-        return ans;
+        else
+        {
+            while (nums[i] != nums[j])
+                i++;
+
+            mp[nums[j]]--;
+            i++;
+        }
+
+        ans = max(ans, j - i);
     }
+
+    return ans;
+}
