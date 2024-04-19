2b94b1db3d6a00ca9ccb62949ec07a834a9f3c02
Abdul-Rahimm <k213951@nu.edu.pk>
2024-04-20 02:00:44 +0500

    number of islands TLE brute force DFS

---
+++ 4. April/19. 200. Number of Islands.cpp
@@ -0,0 +1,66 @@
+// بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) x.begin(), x.end()
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+int rows, cols;
+vector<vector<int>> directions{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+void DFS(int i, int j, vector<vector<char>> grid, vector<vector<bool>> &visited)
+{
+
+    // out of bound
+    if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] == '0' || visited[i][j])
+        return;
+
+    visited[i][j] = true;
+
+    // now explore all directions
+    for (vector<int> dir : directions)
+    {
+        int new_i = i + dir[0];
+        int new_j = j + dir[1];
+
+        DFS(new_i, new_j, grid, visited);
+    }
+
+    return;
+}
+
+int numIslands(vector<vector<char>> &grid)
+{
+    rows = grid.size();
+    cols = grid[0].size();
+    int score = 0;
+
+    vector<vector<bool>> visited(rows, vector<bool>(cols, false));
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+
+            if (grid[i][j] == '1' && visited[i][j] == false)
+            {
+                DFS(i, j, grid, visited);
+                score++;
+            }
+        }
+    }
+
+    return score;
+}
+
+int main()
+{
+    vector<vector<char>> grid{
+        {'1', '1', '1', '1', '0'},
+        {'1', '1', '0', '1', '0'},
+        {'1', '1', '0', '0', '0'},
+        {'1', '1', '0', '0', '0'},
+    };
+
+    cout << numIslands(grid) << endl;
+}
