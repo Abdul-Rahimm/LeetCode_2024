4138d3cc44e286769dfb7343b7ed123a315e2642
Abdul-Rahimm <k213951@nu.edu.pk>
2024-05-24 00:36:19 +0500

    MIK approach. smarter. took a map. inserted an element. explored. removed

--- 5. May/backtracking/23. 2597. The Number of Beautiful Subsets.cpp
+++ 5. May/backtracking/23. 2597. The Number of Beautiful Subsets.cpp
@@ -55,9 +55,40 @@ int beautifulSubsets(vector<int> &nums, int k)
     makeSubsets(nums, 0, k, current);
     return allSubsets.size();
 }
+// -----------------------------------------------------------------------------------------------------
 
-int main()
+int k;
+int result;
+
+void solve(int i, vector<int> &nums, unordered_map<int, int> &mp)
 {
-    vi nums{2, 4, 6};
-    int ans = beautifulSubsets(nums, 2);
+
+    if (i >= nums.size())
+    {
+        result++;
+        return;
+    }
+
+    // not take
+    solve(i + 1, nums, mp);
+
+    // take - with check
+    if (mp[nums[i] + k] == 0 && mp[nums[i] - k] == 0)
+    {
+        mp[nums[i]]++;          // do
+        solve(i + 1, nums, mp); // explore
+        mp[nums[i]]--;          // undo
+    }
+}
+
+int beautifulSubsets(vector<int> &nums, int K)
+{
+    k = K;
+    result = 0;
+
+    unordered_map<int, int> mp;
+
+    solve(0, nums, mp);
+
+    return result - 1;
 }
