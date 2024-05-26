4bf2186ea6ed977d75139e27648ed5bab2218bd7
Abdul-Rahimm <k213951@nu.edu.pk>
2024-05-26 21:24:10 +0500

    accepted approach learned from MIK

--- 5. May/DP/552. Student Attendance Record II.cpp
+++ 5. May/DP/552. Student Attendance Record II.cpp
@@ -71,3 +71,62 @@ int checkRecord(int n)
 {
     return solve(n, "");
 }
+
+// --------------------------------
+// learned from MIK
+
+int mod = 1e9 + 7;
+int dp[100001][2][3];
+
+bool isOkay(string s)
+{
+    int absent = 0;
+    int late = 0;
+
+    for (char i : s)
+    {
+        if (i == 'L')
+            late++;
+        else if (i == 'A')
+        {
+            late = 0;
+            absent++;
+        }
+        else
+        {
+            late = 0;
+        }
+
+        if (late >= 3)
+            return false;
+    }
+
+    if (absent > 1)
+        return false;
+
+    return true;
+}
+
+int solve(int n, int absent, int late)
+{
+    if (absent >= 2 || late >= 3)
+        return 0;
+
+    if (n <= 0)
+        return 1;
+
+    if (dp[n][absent][late] != -1)
+        return dp[n][absent][late];
+
+    int a = solve(n - 1, absent + 1, 0) % mod;
+    int l = solve(n - 1, absent, late + 1) % mod;
+    int p = solve(n - 1, absent, 0) % mod;
+
+    return dp[n][absent][late] = (((a + l) % mod) + p) % mod;
+}
+
+int checkRecord(int n)
+{
+    memset(dp, -1, sizeof(dp));
+    return solve(n, 0, 0);
+}
