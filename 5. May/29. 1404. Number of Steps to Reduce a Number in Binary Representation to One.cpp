7f6b371dc0c3c70e8792770634dfe19450572930
Abdul-Rahimm <k213951@nu.edu.pk>
2024-05-29 23:13:33 +0500

    doesnt cater to "111" types

--- 5. May/29. 1404. Number of Steps to Reduce a Number in Binary Representation to One.cpp
+++ 5. May/29. 1404. Number of Steps to Reduce a Number in Binary Representation to One.cpp
@@ -4,11 +4,11 @@ using namespace std;
 #define all(x) x.begin(), x.end()
 typedef vector<int> vi;
 typedef vector<vi> vii;
-bool isEven(int num)
+bool isEven_outdated(int num)
 {
     return num % 2 == 0;
 }
-int numSteps(string s)
+int numSteps_failed(string s)
 {
     int size = s.size();
     int value = 0;
@@ -27,7 +27,7 @@ int numSteps(string s)
 
     while (value != 1)
     {
-        if (isEven(value))
+        if (isEven_outdated(value))
         {
             value /= 2;
         }
@@ -41,9 +41,62 @@ int numSteps(string s)
 
     return steps;
 }
+// --------------------------------------------------------------------------
+bool isEven(string s)
+{
+    return s[s.size() - 1] == '0';
+}
+void shiftAllRight(string &s)
+{
+    int size = s.size();
+
+    for (int i = size - 2; i >= 0; i--)
+    {
+        s[i + 1] = s[i];
+    }
+
+    s[0] = '0';
+}
+void moveLSBleft(string &s)
+{
+    int i = s.size() - 1;
+    int j = i - 1;
+    bool cont;
+    do
+    {
+        cont = s[j] == '1' ? true : false;
+        s[i] = '0';
+        s[j] = '1';
+        i--;
+        j--;
+    } while (cont);
+}
+int numSteps(string s)
+{
+    int steps = 0;
+
+    while (stoi(s) != 1)
+    {
+        steps++;
+
+        if (isEven(s))
+        {
+            shiftAllRight(s);
+            cout << s << endl;
+        }
+        else
+        {
+            moveLSBleft(s);
+            cout << s << endl;
+        }
+    }
+
+    return steps;
+}
 
 int main()
 {
     string s = "1101";
-    cout << numSteps(s) << endl;
+    int ans = numSteps(s);
+    cout << ans << endl;
 }
