b53f37e2f68283de85a8b602faf30988f4b8b429
Abdul-Rahimm <k213951@nu.edu.pk>
2024-06-09 14:35:31 +0500

    learned approach from MIK

--- 6. June/8. 523. Continuous Subarray Sum.cpp
+++ 6. June/8. 523. Continuous Subarray Sum.cpp
@@ -19,15 +19,46 @@ bool solve(int i, vi &nums, int k, int sum)
     return take | nottake;
 }
 
-bool checkSubarraySum(vector<int> &nums, int k)
+bool checkSubarraySum_notAC(vector<int> &nums, int k)
 {
     int n = nums.size();
 
     return solve(0, nums, k, 0);
 }
 
+bool checkSubarraySum(vector<int> &nums, int k)
+{
+    unordered_map<int, int> mp; // rem --> index
+
+    mp[0] = -1;
+
+    int sum = 0;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+
+        sum += nums[i];
+        int mod = sum % k;
+
+        if (mp.find(mod) == mp.end())
+        {
+            // rem not found in map
+            mp[mod] = i;
+        }
+        else
+        {
+            // remainder found in map
+            // calculate length of subarray
+            if (i - mp[mod] >= 2)
+                return true;
+        }
+    }
+
+    return false;
+}
+
 int main()
 {
-    vi nums{23, 2, 4, 6, 7};
-    cout << checkSubarraySum(nums, 6) << endl;
+    vi nums{23, 2, 6, 4, 7};
+    cout << checkSubarraySum(nums, 13) << endl;
 }
