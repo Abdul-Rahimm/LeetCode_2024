ff8511710e8d7389738c26f159369c143c2e4c87
Abdul-Rahimm <k213951@nu.edu.pk>
2024-08-26 20:13:37 +0500

    binary tree questions

--- 8. August/144. Binary Tree Preorder Traversal.cpp
+++ 8. August/144. Binary Tree Preorder Traversal.cpp
@@ -27,3 +27,29 @@ vector<int> preorderTraversal(TreeNode *root)
 
     return ans;
 }
+
+vector<int> preorderTraversal(TreeNode *root)
+{
+    vector<int> nodes;
+    stack<TreeNode *> todo;
+
+    while (root || !todo.empty())
+    {
+        if (root)
+        {
+            nodes.push_back(root->val);
+            if (root->right)
+            {
+                todo.push(root->right);
+            }
+            root = root->left;
+        }
+        else
+        {
+            root = todo.top();
+            todo.pop();
+        }
+    }
+
+    return nodes;
+}
