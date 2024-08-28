1f8e118ec0378618151209d9f33bb837d6bc5a36
Abdul-Rahimm <k213951@nu.edu.pk>
2024-08-28 15:00:35 +0500

    accepted solution. kinda brute force

--- 8. August/1905. Count Sub Islands.cpp
+++ 8. August/1905. Count Sub Islands.cpp
@@ -1,5 +1,6 @@
 // بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
 #include <bits/stdc++.h>
+#include "../utils.h"
 using namespace std;
 #define all(x) x.begin(), x.end()
 typedef vector<int> vi;
@@ -9,33 +10,45 @@ int n;
 int m;
 vector<vector<int>> directions{{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 
-bool DFS(int i, int j, vector<vector<int>> &grid1, vector<vector<int>> &grid2)
+bool checkValidRegion(vector<vector<int>> &points, vector<vector<int>> &grid1)
+{
+
+    for (auto point : points)
+    {
+        int x = point[0];
+        int y = point[1];
+
+        if (grid1[x][y] == 0)
+            return false;
+    }
+
+    return true;
+}
+
+void DFS(int i, int j, vector<vector<int>> &grid2, vector<vector<int>> &points)
 {
 
     if (i < 0 || i >= n || j < 0 || j >= m || grid2[i][j] == 0)
-        return false;
-
-    if (grid2[i][j] == 1 && grid1[i][j] == 0)
-        return false;
+        return;
 
     // already explored
-    grid1[i][j] = 0;
     grid2[i][j] = 0;
+    points.push_back({i, j});
 
     for (vector<int> dir : directions)
     {
         int new_x = dir[0];
         int new_y = dir[1];
 
-        if (DFS(i + new_x, j + new_y, grid1, grid2))
-            return true;
+        DFS(i + new_x, j + new_y, grid2, points);
     }
 
-    return false;
+    return;
 }
 
 int countSubIslands(vector<vector<int>> &grid1, vector<vector<int>> &grid2)
 {
+
     n = grid1.size();
     m = grid1[0].size();
     int regions = 0;
@@ -45,9 +58,16 @@ int countSubIslands(vector<vector<int>> &grid1, vector<vector<int>> &grid2)
         for (int j = 0; j < m; j++)
         {
 
-            if (grid2[i][j] == 1 && DFS(i, j, grid1, grid2) == true)
+            if (grid2[i][j] == 1)
             {
-                regions++;
+                // coordinates has every single point which has a 1 on grid2 for a particular island
+                vector<vector<int>> coordinates;
+                DFS(i, j, grid2, coordinates);
+
+                // this function will compare the Island coordinates of grid2
+                //  and see if theyre all present in grid1
+                if (checkValidRegion(coordinates, grid1))
+                    regions++;
             }
         }
     }
@@ -60,5 +80,5 @@ int main()
     vii grid1{{1, 1, 1, 0, 0}, {0, 1, 1, 1, 1}, {0, 0, 0, 0, 0}, {1, 0, 0, 0, 0}, {1, 1, 0, 1, 1}};
     vii grid2{{1, 1, 1, 0, 0}, {0, 0, 1, 1, 1}, {0, 1, 0, 0, 0}, {1, 0, 1, 1, 0}, {0, 1, 0, 1, 0}};
     int regions = countSubIslands(grid1, grid2);
-    cout << regions << endl;
+    // cout << regions << endl;
 }
