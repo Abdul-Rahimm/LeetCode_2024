b89b4111e2ee0927aa97548e9f983d9aa6f0a2fc
Abdul-Rahimm <k213951@nu.edu.pk>
2024-10-13 22:28:48 +0500

    contests

---
+++ 10. October/contest cp/C.cpp
@@ -0,0 +1,39 @@
+// بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef array<int, 2> ii;
+typedef vector<ii> vii;
+const int mod = 1e9 + 7;
+
+void solve();
+
+signed main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc = 1;
+    cin >> tc;
+
+    while (tc--)
+        solve();
+
+    return 0;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vi nums(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+}
