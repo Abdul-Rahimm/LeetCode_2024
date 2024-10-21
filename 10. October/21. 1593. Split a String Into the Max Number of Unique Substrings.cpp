3452f547f6391c315f46b0d071887f2265987e2b
Abdul Rahim <k213951@nu.edu.pk>
2024-10-21 20:11:16 +0500

    21 oct. standard backtracking

---
+++ 10. October/21. 1593. Split a String Into the Max Number of Unique Substrings.cpp
@@ -0,0 +1,12 @@
+// بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) x.begin(), x.end()
+typedef vector<int> vi;
+typedef vector<vi> vii;
+
+int main()
+{
+    string s = "12345";
+    cout << s.substr(0, 1) << endl;
+}
