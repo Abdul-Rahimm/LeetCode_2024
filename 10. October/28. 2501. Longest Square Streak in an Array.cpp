11a252f4a6e226f612f9e6aa3ed7c4395f5189b7
Abdul-Rahimm <k213951@nu.edu.pk>
2024-10-28 17:26:46 +0500

    correct approach was using set.

--- 10. October/28. 2501. Longest Square Streak in an Array.cpp
+++ 10. October/28. 2501. Longest Square Streak in an Array.cpp
@@ -5,31 +5,30 @@ using namespace std;
 typedef vector<int> vi;
 typedef vector<vi> vii;
 
-int dp[1001][1001];
-int solve(int i, int prev, vector<int> &nums)
-{
-    if (i >= nums.size())
-        return 0;
-    if (prev != -1 && dp[i][prev + 1] != -1)
-        return dp[i][prev + 1];
-
-    int take = 0;
-    if (prev == -1 || (prev != -1 && nums[prev] * nums[prev] == nums[i]))
-    {
-        take = 1 + solve(i + 1, i, nums);
-    }
-
-    int nottake = solve(i + 1, prev, nums);
-
-    return dp[i][prev + 1] = max(take, nottake);
-}
-
 int longestSquareStreak(vector<int> &nums)
 {
-    sort(nums.begin(), nums.end());
-    memset(dp, -1, sizeof(dp));
+    unordered_set<long long> st(nums.begin(), nums.end());
+    int ans = 0;
 
-    int ans = solve(0, -1, nums);
+    for (int i : nums)
+    {
+        int streak = 1;
+        long long current = i;
+
+        while (true)
+        {
+            long long sq = current * current;
+
+            if (st.find(sq) != st.end())
+            {
+                current = sq;
+                streak++;
+            }
+            else
+                break;
+        }
+        ans = max(ans, streak);
+    }
 
     return ans == 1 ? -1 : ans;
 }
