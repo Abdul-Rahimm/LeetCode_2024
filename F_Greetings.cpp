2970c291a992b54e2b9950384679b8ad6d9e6fa5
Abdul-Rahimm <k213951@nu.edu.pk>
2025-04-05 17:45:40 +0500

    taught knapsackk

--- F_Greetings.cpp
+++ F_Greetings.cpp
@@ -1,101 +1,26 @@
 // بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
 using namespace std;
-typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
-#define pb push_back
 #define all(x) x.begin(), x.end()
-#define int long long
-#define py cout << "YES\n";
-#define pn cout << "NO\n";
-#define forn(a, b) for (int i = a; i < b; i++)
-#define nl cout << endl;
-#define pii pair<int, int>
-typedef long long ll;
 typedef vector<int> vi;
 typedef vector<vi> vii;
-const int mod = 1e9 + 7;
 
-void solve(int i);
-
-signed main(void)
+int recur(int i, int cap, vector<int> &value, vector<int> &weight)
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    int tc = 1;
-    cin >> tc;
+    if (i >= value.size() || weight[i] > cap)
+        return 0;
 
-    for (int i = 1; i <= tc; i++)
-        solve(i);
+    int take = value[i] + recur(i + 1, cap - weight[i], value, weight);
+    int nott = recur(i + 1, cap, value, weight);
 
-    return 0;
+    return max(take, nott);
 }
 
-void solve(int tc)
+int main()
 {
-    // function<int(int, int)> solve = [&](int i, int j) -> int
+    int capacity = 10;
+    vector<int> value{10, 20, 30};
+    vector<int> weight{2, 4, 6};
 
-    int n;
-    cin >> n;
-    vii nums;
-    forn(0, n)
-    {
-        int a, b;
-        cin >> a >> b;
-        nums.push_back({a, b});
-    }
-
-    sort(all(nums));
-    // for (auto row : nums)
-    // {
-    //     for (int i : row)
-    //     {
-    //         cout << i << " ";
-    //     }
-    //     cout << endl;
-    // }
-
-    vii rank;
-
-    for (int i = 0; i < n; i++)
-    {
-        rank.push_back({i, nums[i][1]});
-    }
-
-    // nl;
-    // for (auto row : rank)
-    // {
-    //     for (int i : row)
-    //     {
-    //         cout << i << " ";
-    //     }
-    //     cout << endl;
-    // }
-
-    sort(all(rank), [](vi a, vi b)
-         { return a[1] <= b[1]; });
-
-    // int mn = rank[0][0];
-    indexed_set st;
-    st.insert(rank[0][0]);
-
-    int ans = 0;
-    for (int i = 1; i < n; i++)
-    {
-        // if (rank[i][0] <= mn)
-        // {
-        //     ans += i;
-        //     mn = rank[i][0];
-        // }
-        int temp = 0;
-        temp = st.size() - st.order_of_key(rank[i][0]);
-
-        ans += temp;
-        st.insert(rank[i][0]);
-    }
-
-    cout << ans << endl;
+    cout << recur(0, 10, value, weight);
 }
