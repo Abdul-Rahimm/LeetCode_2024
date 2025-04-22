f07b4b39e7fcbebb233a61902b6170e2688a6823
Abdul-Rahimm <k213951@nu.edu.pk>
2025-04-22 23:26:48 +0500

    huzzi question. reverse LIS

---
+++ C_LIS_or_Reverse_LIS.cpp
@@ -0,0 +1,62 @@
+// بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define int long long
+#define py cout << "YES\n";
+#define pn cout << "NO\n";
+#define forn(a, b) for (int i = a; i < b; i++)
+#define nl cout << endl;
+#define pii pair<int, int>
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+const int mod = 1e9 + 7;
+
+void solve(int i);
+
+signed main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int tc = 1;
+    cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+        solve(i);
+
+    return 0;
+}
+
+void solve(int tc)
+{
+    // function<int(int, int)> solve = [&](int i, int j) -> int
+
+    int n;
+    cin >> n;
+
+    vi nums(n);
+    for (int &i : nums)
+        cin >> i;
+
+    map<int> mp(all(nums));
+    set<int> left, right;
+    for (auto &[a, b] : mp)
+    {
+        left.insert(a);
+        if (b > 1)
+            right.insert(b);
+    }
+    int maxi = max(left.size(), right.size() + 1);
+    
+    while (!st.empty())
+    {
+        auto 
+    }
+}
